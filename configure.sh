671c9272baab9384096cbdc0e2fb21d0d01248b4
Adam Michalunio <amichalunio@gmail.com>
2020-07-23 12:03:55 +0200

    Update configure.sh
    Add NeoVim plugin path

--- configure.sh
+++ configure.sh
@@ -1,2 +1,5 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# for NeoVim
+# curl -fLo ~/.local/share/nvim/site/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
