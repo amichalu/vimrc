a59f85e4eebbf9f5b4da0cafad321de98aaeaf4d
Adam <amichalunio@gmail.com>
2021-04-18 21:38:16 +0200

    more automation ;)

--- configure.sh
+++ configure.sh
@@ -3,3 +3,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 # for NeoVim
 # curl -fLo ~/.local/share/nvim/site/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp .vimrc ~/.vimrc
+
+echo "Now run vim and :PlugInstall"
